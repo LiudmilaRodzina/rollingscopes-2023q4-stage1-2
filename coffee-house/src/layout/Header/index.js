a9b56dbb41141e14ba99e9269109f582b89e4aaf
LiudmilaRodzina <liudmilarodzina@gmail.com>
2023-12-07 15:13:26 +0300

    feat: add burger menu

--- coffee-house/src/layout/Header/index.js
+++ coffee-house/src/layout/Header/index.js
@@ -5,4 +5,28 @@ import "./index.scss";
 
 const header = htmlToElement(Header);
 
+document.addEventListener("DOMContentLoaded", () => {
+  // Burger
+  const burgerBtn = document.querySelector(".burger__container");
+  const menu = document.querySelector(".navigation__wrapper");
+  const navLinks = document.querySelectorAll(".navigation__list_link");
+  const body = document.querySelector("body");
+
+  burgerBtn.addEventListener("click", () => {
+    burgerBtn.classList.toggle("open");
+    menu.classList.toggle("open");
+
+    body.classList.toggle("no-scroll");
+  });
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      burgerBtn.classList.remove("open");
+      menu.classList.remove("open");
+
+      body.classList.remove("no-scroll");
+    });
+  });
+});
+
 export default header;
