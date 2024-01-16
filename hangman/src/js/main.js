b8f73114fa976fbbeebf2c259114bf9cabbe6dcc
LiudmilaRodzina <liudmilarodzina@gmail.com>
2024-01-16 21:40:58 +0300

    refactor: change container height to 100svh

--- hangman/src/js/main.js
+++ hangman/src/js/main.js
@@ -101,17 +101,17 @@ console.log(secretWord);
 
 window.onload = init;
 
-console.log(
-  `Привет! Прости, ещё продолжаю выполнять задание. Если есть возможность, вернись, пожалуйста, к проверке ближе к концу кросс-чека. Спасибо!`
-);
-console.log(`Total: 100
-+ Responsive/adaptive UI from 1440px to 360px viewport: +10
-+ Body in the index.html is empty: +20
-+ The game starts with the correct default view and a random question: +5
-+ The user can play the game by using the virtual keyboard: +20
-+ When the letter is correct, it appears instead of the corresponding underscore. If the letter repeats in the word, all corresponding underscores must be replaced by it: +15
-+ When the letter is incorrect: the incorrect guesses counter is updated: +5
-+ The clicked/pressed letter is disabled: +5
-+ When the user runs out of 6 attempts or wins the game, the modal window appears: +10
-+ The modal window includes the message about the game's outcome (winning or losing), the secret word and the 'play again' button: +10
-`);
+// console.log(
+//   `Привет! Ещё продолжаю выполнять задание. Если есть возможность, вернись, пожалуйста, к проверке ближе к концу кросс-чека. Спасибо!`
+// );
+// console.log(`Total: 100
+// + Responsive/adaptive UI from 1440px to 360px viewport: +10
+// + Body in the index.html is empty: +20
+// + The game starts with the correct default view and a random question: +5
+// + The user can play the game by using the virtual keyboard: +20
+// + When the letter is correct, it appears instead of the corresponding underscore. If the letter repeats in the word, all corresponding underscores must be replaced by it: +15
+// + When the letter is incorrect: the incorrect guesses counter is updated: +5
+// + The clicked/pressed letter is disabled: +5
+// + When the user runs out of 6 attempts or wins the game, the modal window appears: +10
+// + The modal window includes the message about the game's outcome (winning or losing), the secret word and the 'play again' button: +10
+// `);
