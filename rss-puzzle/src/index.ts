5dc1101a583812e2d283cb611749d05307d89896
Liudmila Rodzina <106617515+LiudmilaRodzina@users.noreply.github.com>
2024-03-19 00:05:12 +0300

    feat: (RSS-PZ-01) create input fields and login button on login page (#21)

---
+++ rss-puzzle/src/index.ts
@@ -0,0 +1,19 @@
+import "./index.scss";
+import LoginView from "./view/loginPage/loginPage";
+
+class App {
+  constructor() {
+    App.createView();
+  }
+
+  static createView(): void {
+    const loginView = new LoginView();
+
+    const bodyElement = document.body;
+    if (bodyElement) {
+      bodyElement.append(loginView.getElement() as Node);
+    }
+  }
+}
+const myApp = new App();
+export default myApp;
---
+++ rss-puzzle/src/view/element-creator.ts
@@ -0,0 +1,42 @@
+export default class ElementCreator {
+  private element: HTMLElement | null;
+
+  constructor(
+    params: { tag: string; classNames: string[]; textContent: string } = {
+      tag: "",
+      classNames: [],
+      textContent: "",
+    },
+  ) {
+    this.element = null;
+    this.createElement(params);
+  }
+
+  private createElement(params: {
+    tag: string;
+    classNames: string[];
+    textContent: string;
+  }): void {
+    this.element = document.createElement(params.tag);
+    this.setCSSClasses(params.classNames);
+    this.setTextContent(params.textContent);
+  }
+
+  private setCSSClasses(cssClasses: string[]): void {
+    cssClasses.forEach((className: string) => {
+      if (this.element) {
+        this.element.classList.add(className);
+      }
+    });
+  }
+
+  private setTextContent(text: string): void {
+    if (this.element) {
+      this.element.textContent = text;
+    }
+  }
+
+  public getElement(): HTMLElement | null {
+    return this.element;
+  }
+}
